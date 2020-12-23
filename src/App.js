442818eb1a076d5f6493c225bfba662edb2b8d93
Vinayak Nirvani <vnirvani@gmail.com>
2020-12-23 12:06:01 +0000

    initial setup and added amplify to our project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AmplifyAuthenticator,
+  AmplifySignIn,
+  AmplifySignOut,
+  AmplifySignUp
+} from "@aws-amplify/ui-react";
+import { Center, Heading } from "@chakra-ui/react";
+import { Auth } from "aws-amplify";
+import { SelectMFAType } from "aws-amplify-react";
+import { useEffect, useState } from "react";
+const MFATypes = {
+  SMS: true, // if SMS enabled in your user pool
+  TOTP: true, // if TOTP enabled in your user pool
+  Optional: true, // if MFA is set to optional in your user pool
+};
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    let getUser = async () => {
+      let _user = await Auth.currentAuthenticatedUser();
+      console.log("User: ", _user);
+      setUser(_user);
+    };
+    getUser();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignIn slot="sign-in" usernameAlias="email" />
+        <AmplifySignUp
+          slot="sign-up"
+          usernameAlias="email"
+          formFields={[
+            {
+              type: "given_name",
+              label: "First Name",
+              placeholder: "Your first name",
+              required: true,
+            },
+            {
+              type: "family_name",
+              label: "Last Name",
+              placeholder: "Your last name",
+              required: true,
+            },
+            {
+              type: "email",
+              label: "Email address",
+              placeholder: "Your email address",
+              required: true,
+            },
+            {
+              type: "password",
+              label: "Password",
+              placeholder: "Your password",
+              required: true,
+            },
+            {
+              type: "phone_number",
+              label: "Phone",
+              placeholder: "Your phone number",
+              required: true,
+            },
+          ]}
+        />
+      </AmplifyAuthenticator>
+      <Center h="100vh" as={Heading}>
+        Hello {user?.attributes.given_name}!!!
+        <AmplifySignOut />
+        <SelectMFAType authData={user} MFATypes={MFATypes} />
+      </Center>
+    </>
   );
 }
 
